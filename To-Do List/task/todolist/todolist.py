15c684a0acb70b9f57c327f1721ae03602524794
Já <email@jendakolda.cz>
2020-11-16 22:16:42 +0100

    stage 1 simplified

--- To-Do List/task/todolist/todolist.py
+++ To-Do List/task/todolist/todolist.py
@@ -1,14 +1,14 @@
 # Write your code here
 
 class ToDoList(object):
+    activities = ('Do yoga', 'Make breakfast', 'Learn basics of SQL', 'Learn what is ORM',)
     def __init__(self):
-        self.activities = ('Do yoga', 'Make breakfast', 'Learn basics of SQL', 'Learn what is ORM')
+        pass
 
-    def main(self):
-        print('Today:')
-        for k, v in enumerate(self.activities, start=1):
-            print(f'{k}) {v}')
+    print('Today:')
+    for k, v in enumerate(activities, start=1):
+        print(f'{k}) {v}')
 
 
 if __name__ == '__main__':
-    ToDoList().main()
+    ToDoList()
